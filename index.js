32bf6ae65e24716e796284e182d9aac2799e09a5
Zachary Doherty <zacharyedoherty@gmail.com>
2024-02-26 12:33:08 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
+let random;
+random = Math.floor(Math.random() * 5) + 1;
+
+const num3 = 20;
+const num4 = 4;
+const mod = (num3 % num4) + num4;
+
+const max = Math.max(1,2,4,5,20);
 // Write your code here
